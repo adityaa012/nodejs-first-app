eeab6df76c097c67609bc356dd56b944966a1a0b
Aditya06-ui <147739981+Aditya06-ui@users.noreply.github.com>
2024-05-01 20:01:55 +0530

    fixed engine

--- database/databases.js
+++ database/databases.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 
 const db = () => {
-    mongoose.connect("mongodb://localhost:27017",{
+    mongoose.connect(process.env.mongoose_url,{
     dbname: "childrenraise",
 })
-.then(console.log("database connected"))
+.then((c) => console.log(`database connected + ${c.connection.host}`))
 .catch((e) => console.log("error to connect database" + e))
 }
+
+
 module.exports = db;
